afc709c5f01d98d2585ac3fd704fac8419bdf899
igortega <75392391+igortega@users.noreply.github.com>
2021-05-21 12:16:52 +0200

    download required files at import

--- ultracov/__init__.py
+++ ultracov/__init__.py
@@ -5,22 +5,22 @@ from urllib.request import urlretrieve
 
 here = os.path.split(ultracov.__file__)[0]
 
-# zip_path = os.path.join(here, 'required_files.zip')
-# if not os.path.exists(zip_path):
-#     # download required files
-#     url = r"http://x-cov.com/data/ULTRACOV_UCM_V0.zip"
-#     print('Downloading required files...')
-#     urlretrieve(url, zip_path)
-#
-#     zfile = ZipFile(zip_path)
-#     # extract files to each directory
-#     directories = ['pleura', 'similarity']
-#     for d in directories:
-#         requirements_path = os.path.join(here, d, 'file_requirements.txt')
-#         with open(requirements_path) as f:
-#             files = f.readlines()
-#         for f in files:
-#             fname = f.split('\n')[0]
-#             zfile.extract(fname, path=os.path.join(here, d))
-#
-#     zfile.close()
+zip_path = os.path.join(here, 'required_files.zip')
+if not os.path.exists(zip_path):
+    # download required files
+    url = r"http://x-cov.com/data/ULTRACOV_UCM_V0.zip"
+    print('Downloading required files...')
+    urlretrieve(url, zip_path)
+
+    zfile = ZipFile(zip_path)
+    # extract files to each directory
+    directories = ['pleura', 'similarity']
+    for d in directories:
+        requirements_path = os.path.join(here, d, 'file_requirements.txt')
+        with open(requirements_path) as f:
+            files = f.readlines()
+        for f in files:
+            fname = f.split('\n')[0]
+            zfile.extract(fname, path=os.path.join(here, d))
+
+    zfile.close()
